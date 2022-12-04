2859fe2f5dd4b4684682e981ac37a0e1f7ee9841
Sander in 't Veld <sanderintveld@hotmail.com>
2022-12-04 11:17:16 +0100

    Day 4, part one.

---
+++ src/bin/day04/main.rs
@@ -0,0 +1,65 @@
+/**/
+
+const INPUT: &str = include_str!("input.txt");
+
+pub fn main()
+{
+	println!("Part One: {}", one(INPUT));
+	println!("Part Two: {}", two(INPUT));
+}
+
+fn one(input: &str) -> usize
+{
+	input
+		.lines()
+		.map(|line| line.parse().unwrap())
+		.filter(|pairing| fully_contained_either_way(pairing))
+		.count()
+}
+
+#[derive(parse_display::Display, parse_display::FromStr)]
+#[display("{a},{b}")]
+struct Pairing
+{
+	a: Range,
+	b: Range,
+}
+
+#[derive(parse_display::Display, parse_display::FromStr)]
+#[display("{start}-{end}")]
+struct Range
+{
+	start: i32,
+	end: i32,
+}
+
+fn fully_contained_either_way(pairing: &Pairing) -> bool
+{
+	fully_contains(&pairing.a, &pairing.b)
+		|| fully_contains(&pairing.b, &pairing.a)
+}
+
+fn fully_contains(a: &Range, b: &Range) -> bool
+{
+	(a.start..=a.end).contains(&b.start) && (a.start..=a.end).contains(&b.end)
+}
+
+fn two(_input: &str) -> i32
+{
+	0
+}
+
+#[cfg(test)]
+mod tests
+{
+	use super::*;
+	use pretty_assertions::assert_eq;
+
+	const PROVIDED: &str = include_str!("provided.txt");
+
+	#[test]
+	fn one_provided()
+	{
+		assert_eq!(one(PROVIDED), 2);
+	}
+}
