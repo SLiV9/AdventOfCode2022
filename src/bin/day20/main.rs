40a2820425328f20a37ae3c5688fcb81acd14ee2
Sander in 't Veld <sanderintveld@hotmail.com>
2022-12-20 11:47:14 +0100

    Day 20, part one, second attempt.

--- src/bin/day20/main.rs
+++ src/bin/day20/main.rs
@@ -1,5 +1,7 @@
 /**/
 
+use std::collections::HashSet;
+
 const INPUT: &str = include_str!("input.txt");
 
 pub fn main()
@@ -8,12 +10,39 @@ pub fn main()
 	println!("Part Two: {}", two(INPUT));
 }
 
-fn one(input: &str) -> i16
+fn one(input: &str) -> i32
 {
 	let numbers = input.lines().map(|line| line.parse().unwrap());
-	let commands = input.lines().map(|line| line.parse().unwrap());
-	let mut data: Vec<i16> = numbers.collect();
+	let mut data: Vec<i32> = numbers.collect();
 	let n = data.len();
+	// Force uniqueness while keeping equality modulo n.
+	let min_value = data.iter().min();
+	let max_value = data.iter().max();
+	dbg!(min_value);
+	dbg!(max_value);
+	let k = i32::try_from(10000 * n).unwrap();
+	dbg!(k);
+	let mut set: HashSet<i32> = HashSet::new();
+	for value in data.iter_mut()
+	{
+		while set.insert(*value) == false
+		{
+			if *value > 0
+			{
+				*value += k;
+			}
+			else if *value < 0
+			{
+				*value -= k;
+			}
+			else
+			{
+				panic!("0 must be unique");
+			}
+		}
+	}
+	let commands = data.clone().into_iter();
+	// Perform the mixing.
 	for value in commands
 	{
 		let i = data.iter().position(|v| *v == value).unwrap();
@@ -51,10 +80,11 @@ fn one(input: &str) -> i16
 			}
 		}
 	}
+	// Get the answer.
 	let start = data.iter().position(|v| *v == 0).unwrap();
-	let x = data[(start + 1000) % n];
-	let y = data[(start + 2000) % n];
-	let z = data[(start + 3000) % n];
+	let x = data[(start + 1000) % n] % k;
+	let y = data[(start + 2000) % n] % k;
+	let z = data[(start + 3000) % n] % k;
 	x + y + z
 }
 
@@ -70,10 +100,19 @@ mod tests
 	use pretty_assertions::assert_eq;
 
 	const PROVIDED: &str = include_str!("provided.txt");
+	const DUPLICATE: &str = include_str!("duplicate.txt");
 
 	#[test]
 	fn one_provided()
 	{
 		assert_eq!(one(PROVIDED), 3);
+	}
+
+	#[test]
+	fn one_duplicate()
+	{
+		// 1, 2, -3, 0, 3, -3, -2
+		// 1, 2, -3, -3, 0, 3, -2
+		assert_eq!(one(DUPLICATE), -4);
 	}
 }
