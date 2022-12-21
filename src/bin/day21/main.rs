5e68efcd81d4a23953b690c40d65c9a7f7dc6f97
Sander in 't Veld <sanderintveld@hotmail.com>
2022-12-21 16:45:03 +0100

    Day 21, part two.

--- src/bin/day21/main.rs
+++ src/bin/day21/main.rs
@@ -22,13 +22,70 @@ fn one(input: &str) -> i64
 		computation.handle(monkey);
 		computation.resolve();
 	}
-	//dbg!(&computation);
-	computation.answer()
+	let root: Name = "root".parse().unwrap();
+	computation.answer(root).unwrap()
 }
 
-fn two(_input: &str) -> i32
+fn two(input: &str) -> i64
 {
-	0
+	let root: Name = "root".parse().unwrap();
+	let humn: Name = "humn".parse().unwrap();
+	let monkeys = input.lines().map(|line| line.parse().unwrap());
+	let monkeys = monkeys.map(|monkey| correct(monkey, root, humn));
+	let mut computation = Computation::default();
+	computation.store(root, 0);
+	for monkey in monkeys
+	{
+		computation.handle(monkey);
+		computation.resolve();
+	}
+	computation.answer(humn).unwrap_or_else(|| {
+		dbg!(&computation);
+		computation.find_resolution_issue();
+		panic!("unresolved");
+	})
+}
+
+fn correct(monkey: Monkey, root: Name, humn: Name) -> Monkey
+{
+	if monkey.name == root
+	{
+		match monkey.expr
+		{
+			Expression::Operation { op: _, left, right } => Monkey {
+				name: root,
+				expr: Expression::SolveOperation {
+					op: Operator::Sub,
+					left,
+					right,
+				},
+			},
+			_ => unreachable!(),
+		}
+	}
+	else if monkey.name == humn
+	{
+		Monkey {
+			name: humn,
+			expr: Expression::Null,
+		}
+	}
+	else
+	{
+		let expr = match monkey.expr
+		{
+			Expression::Constant(_) => monkey.expr,
+			Expression::Operation { op, left, right } =>
+			{
+				Expression::SolveOperation { op, left, right }
+			}
+			_ => unreachable!(),
+		};
+		Monkey {
+			name: monkey.name,
+			expr,
+		}
+	}
 }
 
 #[derive(Debug, Default)]
@@ -46,40 +103,114 @@ impl Computation
 	{
 		match monkey.expr
 		{
-			Expression::Null => unreachable!(),
+			Expression::Null => (),
 			Expression::Constant(answer) => self.store(monkey.name, answer),
 			Expression::Operation { op, left, right } =>
 			{
-				let result = self.query(left, right);
-				match result
+				let waiting = WaitingMonkey {
+					name: monkey.name,
+					op,
+					left,
+					right,
+				};
+				self.handle_waiting(waiting)
+			}
+			Expression::SolveOperation { op, left, right } =>
+			{
+				let waiting = WaitingMonkey {
+					name: monkey.name,
+					op,
+					left,
+					right,
+				};
+				self.handle_unsolved(waiting)
+			}
+		}
+	}
+
+	fn handle_waiting(&mut self, monkey: WaitingMonkey)
+	{
+		let result = self.query(monkey.left, monkey.right);
+		match result
+		{
+			QueryResult::Resolved { left, right } =>
+			{
+				let answer = calculate(monkey.op, left, right);
+				self.store(monkey.name, answer);
+			}
+			QueryResult::WaitingForLeft | QueryResult::WaitingForRight =>
+			{
+				self.waiting_for_one.push(monkey);
+			}
+			QueryResult::WaitingForBoth =>
+			{
+				self.waiting_for_two.push(monkey);
+			}
+		}
+	}
+
+	fn handle_unsolved(&mut self, monkey: WaitingMonkey)
+	{
+		let result = self.query(monkey.left, monkey.right);
+		match result
+		{
+			QueryResult::Resolved { left, right } =>
+			{
+				let answer = calculate(monkey.op, left, right);
+				self.store(monkey.name, answer);
+			}
+			QueryResult::WaitingForLeft =>
+			{
+				if self.answer(monkey.name).is_some()
 				{
-					QueryResult::Resolved { left, right } =>
-					{
-						let answer = calculate(op, left, right);
-						self.store(monkey.name, answer);
-					}
-					QueryResult::WaitingForOne =>
-					{
-						let monkey = WaitingMonkey {
-							name: monkey.name,
-							op,
-							left,
-							right,
-						};
-						self.waiting_for_one.push(monkey);
-					}
-					QueryResult::WaitingForTwo =>
-					{
-						let monkey = WaitingMonkey {
-							name: monkey.name,
-							op,
-							left,
-							right,
-						};
-						self.waiting_for_two.push(monkey);
-					}
+					self.handle_solved(monkey.solve_left());
+				}
+				else
+				{
+					self.waiting_for_one.push(monkey.solve_left());
+					self.waiting_for_one.push(monkey);
 				}
 			}
+			QueryResult::WaitingForRight =>
+			{
+				if self.answer(monkey.name).is_some()
+				{
+					self.handle_solved(monkey.solve_right());
+				}
+				else
+				{
+					self.waiting_for_one.push(monkey.solve_right());
+					self.waiting_for_one.push(monkey);
+				}
+			}
+			QueryResult::WaitingForBoth =>
+			{
+				if self.answer(monkey.name).is_some()
+				{
+					self.waiting_for_one.push(monkey.solve_left());
+					self.waiting_for_one.push(monkey.solve_right());
+				}
+				else
+				{
+					self.waiting_for_two.push(monkey.solve_left());
+					self.waiting_for_two.push(monkey.solve_right());
+					self.waiting_for_two.push(monkey);
+				}
+			}
+		}
+	}
+
+	fn handle_solved(&mut self, monkey: WaitingMonkey)
+	{
+		let result = self.query(monkey.left, monkey.right);
+		match result
+		{
+			QueryResult::Resolved { left, right } =>
+			{
+				let answer = calculate(monkey.op, left, right);
+				self.store(monkey.name, answer);
+			}
+			_ => unreachable!(),
 		}
 	}
 
@@ -113,7 +244,11 @@ impl Computation
 
 	fn store(&mut self, name: Name, answer: i64)
 	{
-		self.answers.insert(name.into(), answer);
+		let old = self.answers.insert(name.into(), answer);
+		if let Some(old_answer) = old
+		{
+			assert_eq!(old_answer, answer);
+		}
 		self.resolved_names.push_back(name);
 	}
 
@@ -124,20 +259,46 @@ impl Computation
 		match (left, right)
 		{
 			(Some(left), Some(right)) => QueryResult::Resolved { left, right },
-			(Some(_), None) => QueryResult::WaitingForOne,
-			(None, Some(_)) => QueryResult::WaitingForOne,
-			(None, None) => QueryResult::WaitingForTwo,
+			(Some(_), None) => QueryResult::WaitingForRight,
+			(None, Some(_)) => QueryResult::WaitingForLeft,
+			(None, None) => QueryResult::WaitingForBoth,
 		}
 	}
 
-	fn answer(&self) -> i64
+	fn answer(&self, name: Name) -> Option<i64>
 	{
-		let root: Name = "root".parse().unwrap();
-		self.answers.get(&root.into()).cloned().unwrap()
+		self.answers.get(&name.into()).cloned()
+	}
+
+	fn find_resolution_issue(&self)
+	{
+		for monkey in &self.waiting_for_one
+		{
+			match self.query(monkey.left, monkey.right)
+			{
+				QueryResult::Resolved { .. } =>
+				{
+					panic!("not resolved in time: {:?}", monkey)
+				}
+				QueryResult::WaitingForLeft => (),
+				QueryResult::WaitingForRight => (),
+				QueryResult::WaitingForBoth => unreachable!(),
+			}
+		}
+		for monkey in &self.waiting_for_two
+		{
+			match self.query(monkey.left, monkey.right)
+			{
+				QueryResult::Resolved { .. } => unreachable!(),
+				QueryResult::WaitingForLeft => unreachable!(),
+				QueryResult::WaitingForRight => unreachable!(),
+				QueryResult::WaitingForBoth => (),
+			}
+		}
 	}
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Copy)]
 struct WaitingMonkey
 {
 	name: Name,
@@ -146,11 +307,52 @@ struct WaitingMonkey
 	right: Name,
 }
 
+impl std::fmt::Debug for WaitingMonkey
+{
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error>
+	{
+		write!(
+			f,
+			"\"{} <- {} {} {}\"",
+			self.name, self.left, self.op, self.right
+		)
+	}
+}
+
 impl WaitingMonkey
 {
 	fn is_waiting_for(&self, other: Name) -> bool
 	{
 		self.left == other || self.right == other
+	}
+
+	fn solve_left(self) -> Self
+	{
+		WaitingMonkey {
+			name: self.left,
+			op: self.op.inverted(),
+			left: self.name,
+			right: self.right,
+		}
+	}
+
+	fn solve_right(self) -> Self
+	{
+		match self.op
+		{
+			Operator::Add | Operator::Mul => WaitingMonkey {
+				name: self.right,
+				op: self.op.inverted(),
+				left: self.name,
+				right: self.left,
+			},
+			Operator::Sub | Operator::Div => WaitingMonkey {
+				name: self.right,
+				op: self.op,
+				left: self.left,
+				right: self.name,
+			},
+		}
 	}
 }
 
@@ -162,8 +364,9 @@ enum QueryResult
 		left: i64,
 		right: i64,
 	},
-	WaitingForOne,
-	WaitingForTwo,
+	WaitingForLeft,
+	WaitingForRight,
+	WaitingForBoth,
 }
 
 #[derive(Debug, Default, Clone, Copy)]
@@ -191,6 +394,13 @@ enum Expression
 		left: Name,
 		right: Name,
 	},
+	#[display("solve ({left} {op} {right})")]
+	SolveOperation
+	{
+		op: Operator,
+		left: Name,
+		right: Name,
+	},
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -206,6 +416,20 @@ enum Operator
 	Mul,
 	#[display("/")]
 	Div,
+}
+
+impl Operator
+{
+	fn inverted(self) -> Self
+	{
+		match self
+		{
+			Operator::Add => Operator::Sub,
+			Operator::Sub => Operator::Add,
+			Operator::Mul => Operator::Div,
+			Operator::Div => Operator::Mul,
+		}
+	}
 }
 
 fn calculate(op: Operator, left: i64, right: i64) -> i64
@@ -294,8 +518,20 @@ mod tests
 	}
 
 	#[test]
+	fn two_provided()
+	{
+		assert_eq!(two(PROVIDED), 301);
+	}
+
+	#[test]
 	fn one_mini()
 	{
 		assert_eq!(one(MINI), 200);
+	}
+
+	#[test]
+	fn two_mini()
+	{
+		assert_eq!(two(MINI), 50);
 	}
 }
