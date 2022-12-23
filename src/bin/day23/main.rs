276b97a8b5759d9e3c22d45c04e2ba0626be2777
Sander in 't Veld <sanderintveld@hotmail.com>
2022-12-23 15:46:44 +0100

    Day 23, part one, provided example works but running the real input causes elves to disappear.

--- src/bin/day23/main.rs
+++ src/bin/day23/main.rs
@@ -8,28 +8,132 @@ pub fn main()
 	println!("Part Two: {}", two(INPUT));
 }
 
+// Instead of east and west, use LESS significant and MORE significant.
 const PROPOSAL_SEQUENCE_LEN: usize = 4;
-const PROPOSAL_SEQUENCE: [u8; PROPOSAL_SEQUENCE_LEN] = [b'N', b'S', b'W', b'E'];
+const PROPOSAL_SEQUENCE: [u8; PROPOSAL_SEQUENCE_LEN] = [b'N', b'S', b'L', b'M'];
 
 fn one(input: &str) -> usize
 {
 	let mut grid = Grid::create();
 	grid.parse_input(input);
-	while grid.is_active
+	grid.dbg_print();
+	let count = grid.count();
+	for round in 1..=10
 	{
+		println!("Round {}", round);
 		grid.expand();
+		grid.dbg_print();
 		grid.diffuse();
+		grid.dbg_print();
 		grid.collapse();
+		assert_eq!(grid.count(), count);
+		if !grid.is_active
+		{
+			break;
+		}
 	}
+	println!("Done");
+	grid.dbg_print();
 	grid.count_empty_spaces()
 }
 
-fn two(_input: &str) -> i32
+fn two(input: &str) -> usize
 {
-	0
+	let mut grid = Grid::create();
+	grid.parse_input(input);
+	grid.dbg_print();
+	let count = grid.count();
+	let mut round = 1;
+	while grid.is_active
+	{
+		println!("Round {}", round);
+		grid.expand();
+		grid.dbg_print();
+		grid.diffuse();
+		grid.dbg_print();
+		grid.collapse();
+		assert_eq!(grid.count(), count);
+		round += 1;
+	}
+	println!("Done");
+	grid.dbg_print();
+	grid.count_empty_spaces()
 }
 
-const MAX_ROWS: usize = 128;
+fn propose(
+	current: u128,
+	above: u128,
+	below: u128,
+	proposal_sequence: &[u8; PROPOSAL_SEQUENCE_LEN],
+) -> Proposal
+{
+	if current == 0
+	{
+		return Proposal::default();
+	}
+	let l = current << 1;
+	let m = current >> 1;
+	let above3 = above | (above >> 1) | (above << 1);
+	let below3 = below | (below >> 1) | (below << 1);
+	let happy = current & !l & !m & !above3 & !below3;
+	let mut unhappy = current & !happy;
+	let mut proposal = Proposal::default();
+	for proposed_direction in proposal_sequence.iter().map(|x| *x)
+	{
+		match proposed_direction
+		{
+			b'N' =>
+			{
+				proposal.north = unhappy & !above3;
+				unhappy &= !proposal.north;
+			}
+			b'S' =>
+			{
+				proposal.south = unhappy & !below3;
+				unhappy &= !proposal.south;
+			}
+			b'L' =>
+			{
+				let from = unhappy & !l & !(above << 1) & !(below << 1);
+				proposal.less = from >> 1;
+				unhappy &= !from;
+			}
+			b'M' =>
+			{
+				let from = unhappy & !m & !(above >> 1) & !(below >> 1);
+				proposal.more = from << 1;
+				unhappy &= !from;
+			}
+			_ => unreachable!(),
+		}
+	}
+	proposal.stay = happy | unhappy;
+	proposal
+}
+
+#[derive(Debug, Default, Clone)]
+struct Proposal
+{
+	stay: u128,
+	north: u128,
+	south: u128,
+	less: u128,
+	more: u128,
+}
+
+fn block(a: u128, b: u128, c: u128, d: u128) -> u128
+{
+	(a & (b | c | d)) | (b & (c | d)) | (c & d)
+}
+
+fn resolve_block(blocked: u128, proposed: &mut u128, backup: &mut u128)
+{
+	let canceled = blocked & *proposed;
+	*proposed &= !canceled;
+	*backup |= canceled;
+}
+
+const MAX_ROWS: usize = 128 + 1;
 const MAX_COLS: usize = 128;
 
 struct Grid
@@ -72,21 +176,21 @@ impl Grid
 	fn set_at_rc(&mut self, r: usize, c: usize)
 	{
 		self.data[r] |= 1u128 << c;
-		if r > self.height
+		if r + 1 > self.height
 		{
-			self.height = r;
+			self.height = r + 1;
 		}
-		if c > self.width
+		if c + 1 > self.width
 		{
-			self.width = c;
+			self.width = c + 1;
 		}
 	}
 
 	fn expand(&mut self)
 	{
-		// Add an empty row at the top and the bottom.
+		// Add an empty row at the bottom and two at the top.
 		self.data[0..(self.height + 1)].rotate_left(self.height);
-		self.height += 2;
+		self.height += 3;
 		assert!(self.height <= MAX_ROWS);
 		// Add an empty column at the start and the end.
 		for row in &mut self.data
@@ -100,21 +204,53 @@ impl Grid
 	fn diffuse(&mut self)
 	{
 		self.is_active = false;
-		let mut proposed_previous_line: u128 = 0;
-		for r in 0..self.height
+		assert!(self.height >= 3);
+		let mut prev = Proposal::default();
+		let mut curr = propose(
+			self.data[1],
+			self.data[0],
+			self.data[2],
+			&self.proposal_sequence,
+		);
+		self.data[0] = curr.north;
+		for r_of_prev in 0..(self.height - 3)
 		{
-			// TODO implement some weird scan line algorithm
+			let r_of_current = r_of_prev + 1;
+			let r_of_next = r_of_prev + 2;
+			let mut next = propose(
+				self.data[r_of_next],
+				self.data[r_of_next - 1],
+				self.data[r_of_next + 1],
+				&self.proposal_sequence,
+			);
+			let blocked = block(prev.south, curr.less, curr.more, next.north);
+			resolve_block(blocked, &mut prev.south, &mut self.data[r_of_prev]);
+			resolve_block(blocked, &mut curr.less, &mut curr.stay);
+			resolve_block(blocked, &mut curr.more, &mut curr.stay);
+			resolve_block(blocked, &mut next.north, &mut next.stay);
+			let arrived = prev.south | curr.less | curr.more | next.north;
+			if arrived != 0
+			{
+				self.is_active = true;
+			}
+			self.data[r_of_current] = curr.stay | arrived;
+			prev = curr;
+			curr = next;
 		}
+		self.data[self.height - 2] |= prev.south;
+		assert_eq!(curr.less, 0);
+		assert_eq!(curr.stay, 0);
+		assert_eq!(curr.more, 0);
 		self.proposal_sequence.rotate_left(1);
 	}
 
 	fn collapse(&mut self)
 	{
 		// Trim empty rows.
-		let from = self.data.iter().position(|&row| row != 0).unwrap();
-		let to = self.data.iter().rposition(|&row| row != 0).unwrap();
-		self.data[0..to].rotate_left(from);
-		self.height = to - from;
+		let start = self.data.iter().position(|&row| row != 0).unwrap();
+		let end = self.data.iter().rposition(|&row| row != 0).unwrap() + 1;
+		self.data[0..end].rotate_left(start);
+		self.height = end - start;
 		// Trim empty columns.
 		let mask = self.data[0..self.height].iter().fold(0, |a, row| (a | row));
 		let n = mask.trailing_zeros();
@@ -125,12 +261,45 @@ impl Grid
 		self.width = MAX_COLS - n as usize - mask.leading_zeros() as usize;
 	}
 
+	fn count(&self) -> usize
+	{
+		self.data[0..self.height]
+			.iter()
+			.map(|word| word.count_ones() as usize)
+			.sum()
+	}
+
 	fn count_empty_spaces(&self) -> usize
 	{
 		self.data[0..self.height]
 			.iter()
 			.map(|word| self.width - word.count_ones() as usize)
 			.sum()
+	}
+
+	#[allow(unused)]
+	fn dbg_print(&self)
+	{
+		println!();
+		for row in &self.data[0..self.height]
+		{
+			print!("| ");
+			let mut mask = *row;
+			for c in 0..self.width
+			{
+				if mask & 0x01 != 0
+				{
+					print!("#");
+				}
+				else
+				{
+					print!(".");
+				}
+				mask >>= 1;
+			}
+			println!();
+		}
+		println!();
 	}
 }
 
@@ -141,10 +310,17 @@ mod tests
 	use pretty_assertions::assert_eq;
 
 	const PROVIDED: &str = include_str!("provided.txt");
+	const MINI: &str = include_str!("mini.txt");
 
 	#[test]
 	fn one_provided()
 	{
 		assert_eq!(one(PROVIDED), 110);
+	}
+
+	#[test]
+	fn one_mini()
+	{
+		assert_eq!(one(MINI), 25);
 	}
 }
